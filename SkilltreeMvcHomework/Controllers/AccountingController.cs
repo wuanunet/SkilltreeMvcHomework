8a4910eae18473c8c482ecce6f081a66712945b2
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-04-12 01:54:26 +0800

    BTS002 - 新增 AccountingController。

---
+++ SkilltreeMvcHomework/Controllers/AccountingController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace SkilltreeMvcHomework.Controllers
+{
+    public class AccountingController : Controller
+    {
+        public ActionResult Manage()
+        {
+            return View();
+        }
+    }
+}
