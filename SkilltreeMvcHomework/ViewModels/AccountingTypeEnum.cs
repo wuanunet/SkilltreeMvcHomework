b8795e2d7c0620899619a12cd8f5adb6d5ab7180
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-04-12 02:42:47 +0800

    BTS003 - 1.調整 _Layout。2.新增 Accounting Manage View。3.新增 AccountingViewModel, TypeEnum。

---
+++ SkilltreeMvcHomework/ViewModels/AccountingTypeEnum.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkilltreeMvcHomework.ViewModels
+{
+    public enum AccountingTypeEnum
+    {
+        [Display(Name = "支出")]
+        Expend = 0,
+
+        [Display(Name = "收入")]
+        Income = 1
+    }
+}
---
+++ SkilltreeMvcHomework/ViewModels/AccountingViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SkilltreeMvcHomework.ViewModels
+{
+    public class AccountingViewModel
+    {
+        [Range(1, int.MaxValue, ErrorMessage = "請至少選擇一項")]
+        [Display(Name = "類別")]
+        public AccountingTypeEnum Type { get; set; }
+
+        [Display(Name = "金額")]
+        public int Cost { get; set; }
+
+        [Display(Name = "備註")]
+        public string Remark { get; set; }
+
+        [Display(Name = "日期")]
+        public DateTime CreateTime { get; set; }
+    }
+}
