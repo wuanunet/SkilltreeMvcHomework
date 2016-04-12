9e4d34b7e8794da46785bfaaea27f002c4f4e405
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-04-12 11:16:37 +0800

    BTS006 - AccountingViewModel 加入驗證項目。

--- SkilltreeMvcHomework/ViewModels/AccountingViewModel.cs
+++ SkilltreeMvcHomework/ViewModels/AccountingViewModel.cs
@@ -5,16 +5,18 @@ namespace SkilltreeMvcHomework.ViewModels
 {
     public class AccountingViewModel
     {
-        [Range(1, int.MaxValue, ErrorMessage = "請至少選擇一項")]
         [Display(Name = "類別")]
         public AccountingTypeEnum Type { get; set; }
 
+        [Required]
         [Display(Name = "金額")]
         public int Cost { get; set; }
 
         [Display(Name = "備註")]
         public string Remark { get; set; }
 
+        [Required]
+        [UIHint("DateTime")]
         [Display(Name = "日期")]
         public DateTime CreateTime { get; set; }
     }
