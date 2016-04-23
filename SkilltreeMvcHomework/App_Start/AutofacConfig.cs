934f067a606b81c882b8f6bc830e05339c29c525
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-04-23 14:26:12 +0800

    1.專案加入 Autofac 組件。2.加入 AutofacConfig。

---
+++ SkilltreeMvcHomework/App_Start/AutofacConfig.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.Mvc;
+using SkilltreeMvcHomework.Repositories;
+using SkilltreeMvcHomework.Services;
+using System.Web.Mvc;
+
+namespace SkilltreeMvcHomework
+{
+    public class AutofacConfig
+    {
+        public static void Initialize()
+        {
+            var builder = new ContainerBuilder();
+
+            builder.RegisterControllers(typeof(MvcApplication).Assembly);
+
+            //// Repositories
+            builder.RegisterType<AccountingRepository>().As<IAccountingRepository>();
+
+            //// Services
+            builder.RegisterType<AccountingService>().As<IAccountingService>();
+
+            var container = builder.Build();
+
+            DependencyResolver.SetResolver(new AutofacDependencyResolver(container));
+        }
+    }
+}
