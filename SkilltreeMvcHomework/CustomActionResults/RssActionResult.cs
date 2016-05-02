3f34fa157275272d198f2f73efa3b281dcc4aae5
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-05-03 00:03:26 +0800

    1.加入 System.ServiceModel 組件參考。2.新增 RssActionResult。

---
+++ SkilltreeMvcHomework/CustomActionResults/RssActionResult.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel.Syndication;
+using System.Web.Mvc;
+using System.Xml;
+
+namespace SkilltreeMvcHomework.CustomActionResults
+{
+    public class RssActionResult : ActionResult
+    {
+        private SyndicationFeed feed;
+
+        public RssActionResult(SyndicationFeed feed)
+        {
+            this.feed = feed;
+        }
+
+        public override void ExecuteResult(ControllerContext context)
+        {
+            context.HttpContext.Response.ContentType = "application/rss+xml";
+            Rss20FeedFormatter formatter = new Rss20FeedFormatter(this.feed);
+            using (XmlWriter writer = XmlWriter.Create(context.HttpContext.Response.Output))
+            {
+                formatter.WriteTo(writer);
+            }
+        }
+    }
+}
