22db34a40ba6c92fd0f38c0802b776593d34ae65
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-05-07 14:42:38 +0800

    初始化後台記帳管理功能。(先把前台功能搬過來，之後再調整)

---
+++ SkilltreeMvcHomework/Areas/Backend/Controllers/AccountingBookController.cs
@@ -0,0 +1,44 @@
+﻿using SkilltreeMvcHomework.Services;
+using SkilltreeMvcHomework.ViewModels;
+using System.Web.Mvc;
+
+namespace SkilltreeMvcHomework.Areas.Backend.Controllers
+{
+    public class AccountingBookController : BaseController
+    {
+        private readonly IAccountingService _accountingService;
+
+        public AccountingBookController(IAccountingService accountingService)
+        {
+            this._accountingService = accountingService;
+        }
+
+        public ActionResult Manage()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Manage(AccountingViewModel pageData)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(pageData);
+            }
+
+            var result = this._accountingService.Create(pageData);
+
+            var data = this._accountingService.GetData();
+
+            return PartialView("History", data);
+        }
+
+        [ChildActionOnly]
+        public ActionResult History()
+        {
+            var result = this._accountingService.GetData();
+
+            return View(result);
+        }
+    }
+}
---
+++ SkilltreeMvcHomework/Areas/Backend/Controllers/BaseController.cs
@@ -0,0 +1,9 @@
+﻿using System.Web.Mvc;
+
+namespace SkilltreeMvcHomework.Areas.Backend.Controllers
+{
+    [Authorize(Users = "wuanunet@hotmail.com")]
+    public class BaseController : Controller
+    {
+    }
+}
