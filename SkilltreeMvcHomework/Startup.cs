8949977bb7e7dea95b4d9308c685132486e8b77b
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-05-07 13:32:43 +0800

    1.將 Asp.Net Identity 組件與相關程式加入專案。2.升級所有組件版本到最新。

---
+++ SkilltreeMvcHomework/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SkilltreeMvcHomework.Startup))]
+
+namespace SkilltreeMvcHomework
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
