933da7a0ec54a0de203bc7196d038159524a7065
Cheng-Ju Wu <wuanunet@hotmail.com>
2016-05-07 17:04:31 +0800

    加入 Query 的擴充類別。

---
+++ SkilltreeMvcHomework/ViewModels/PagingOptions.cs
@@ -0,0 +1,24 @@
+﻿using PagedList.Mvc;
+
+namespace SkilltreeMvcHomework.ViewModels
+{
+    public class PagingOptions
+    {
+        public static PagedListRenderOptions Standard
+        {
+            get
+            {
+                var options = new PagedListRenderOptions
+                {
+                    DisplayLinkToFirstPage = PagedListDisplayMode.Always,
+                    DisplayLinkToPreviousPage = PagedListDisplayMode.Always,
+                    LinkToPreviousPageFormat = "上一頁",
+                    DisplayLinkToNextPage = PagedListDisplayMode.Always,
+                    LinkToNextPageFormat = "下一頁",
+                    DisplayLinkToLastPage = PagedListDisplayMode.Always
+                };
+                return options;
+            }
+        }
+    }
+}
